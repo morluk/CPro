cd0d63430ddda3666aa7d31fab2b235b799b209c
Lukas <gugugs@x300-lukas.fritz.box>
2014-04-02 15:53:41 +0200

    first commit

---
+++ netbeans_project/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: gugugs
+ *
+ * Created on 2. April 2014, 15:27
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
